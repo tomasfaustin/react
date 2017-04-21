f9dbec79b2c5b1a30d34900a44c44b1c8ac1c6e1
Thomas Huisking <tomasfaustin@gmail.com>
2017-04-21 11:34:06 -0700

    Update files

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,21 @@
+var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'React'
+    };
+  },
+  render: function() {
+    var name = this.props.name;
+    return (
+      <div>
+        <h1>Hello, {name}!</h1>
+        <p>This is from the component! Can you even believe how jsx works?</p>
+      </div>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello, React!</h1>,
+  <Greeter name="Thomas"/>,
   document.getElementById('app')
 );
