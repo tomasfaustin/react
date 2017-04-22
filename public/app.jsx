cfb167d2f0c703c4d9a7ec84d83a12a76e30bbdf
Thomas Huisking <tomasfaustin@gmail.com>
2017-04-22 15:37:10 -0700

    Update nested components on app.jsx

--- public/app.jsx
+++ public/app.jsx
@@ -1,20 +1,41 @@
+var GreeterMessage = React.createClass({
+    render: function () {
+      return (
+        <div>
+          <h1>Some H1</h1>
+          <p>Some paragraph</p>
+        </div>
+      );
+    }
+});
+
+var GreeterForm = React.createClass({
+  render: function () {
+    return (
+      <form>
+        <input type="text" ref="name"/>
+        <button>Set Name</button>
+      </form>
+    );
+  }
+});
+
 var Greeter = React.createClass({
-  getDefaultProps: function() {
+  getDefaultProps: function () {
     return {
       name: 'React',
-      message: 'This is the default message.'
+      message: 'This is the default message!'
     };
   },
   getInitialState: function () {
     return {
-      name: this.props.name
-    }
+        name: this.props.name
+    };
   },
   onButtonClick: function (e) {
     e.preventDefault();
 
     var nameRef = this.refs.name;
-
     var name = nameRef.value;
     nameRef.value = '';
 
@@ -24,26 +45,31 @@ var Greeter = React.createClass({
       });
     }
   },
-  render: function() {
+  render: function () {
     var name = this.state.name;
     var message = this.props.message;
 
     return (
       <div>
-        <h1>Hello, {name}!</h1>
-        <p>{message + '!!'} Can you even believe how jsx works?</p>
+        <h1>Hello {name}!</h1>
+        <p>{message + '!!'}</p>
+
+        <GreeterMessage/>
 
         <form onSubmit={this.onButtonClick}>
           <input type="text" ref="name"/>
           <button>Set Name</button>
         </form>
+
+        <GreeterForm/>
       </div>
     );
   }
 });
 
+var firstName = 'Thomas Bo-fucking-Bombous';
 
 ReactDOM.render(
-  <Greeter name="Thomas" message="Message from prop!"/>,
+  <Greeter name={firstName}/>,
   document.getElementById('app')
 );
