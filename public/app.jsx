39205b384f633b7943a3165a5518dc5d95411950
Thomas Huisking <tomasfaustin@gmail.com>
2017-04-22 13:24:05 -0700

    Update app.jsx with getDefaultProps method

--- public/app.jsx
+++ public/app.jsx
@@ -1,21 +1,25 @@
 var Greeter = React.createClass({
   getDefaultProps: function() {
     return {
-      name: 'React'
+      name: 'React',
+      message: 'This is the default message.'
     };
   },
   render: function() {
     var name = this.props.name;
+    var message = this.props.message;
+
     return (
       <div>
         <h1>Hello, {name}!</h1>
-        <p>This is from the component! Can you even believe how jsx works?</p>
+        <p>{message + '!!'} Can you even believe how jsx works?</p>
       </div>
     );
   }
 });
 
+
 ReactDOM.render(
-  <Greeter name="Thomas"/>,
+  <Greeter name="Thomas" message="Message from prop!"/>,
   document.getElementById('app')
 );
