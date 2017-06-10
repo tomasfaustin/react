546d4e6073352585f1ede50aef9c1dc995b60a67
Thomas Huisking <tomasfaustin@gmail.com>
2017-06-10 05:58:38 -0700

    Them promises tho

--- reactWeather/example-promise.js
+++ reactWeather/example-promise.js
@@ -1,25 +1,53 @@
-function getTempCallback (location, callback) {
-  callback(undefined, 78);
-  callback('City not found');
-}
+// Those promises tho!
 
-getTempCallback('Philadelphia', function (err, temp) {
-  if (err) {
-    console.log('error', err);
-  } else {
-    console.log('success', temp)
-  }
-});
+// function getTempCallback (location, callback) {
+//   callback(undefined, 78);
+//   callback('City not found');
+// }
+//
+// getTempCallback('Philadelphia', function (err, temp) {
+//   if (err) {
+//     console.log('error', err);
+//   } else {
+//     console.log('success', temp)
+//   }
+// });
+//
+// function getTempPromise (location) {
+//   return new Promise(function (resolve, reject) {
+//     setTimeout(function () {
+//     resolve(79);
+//     reject('City not found');
+//     }, 1000);
+//   });
+// }
+//
+// getTempPromise('Philadelphia').then(function (temp) {
+//   console.log('promise success', temp);
+// }, function (err) {
+//   console.log('promise error', err);
+// })
 
-function getTempPromise (location) {
-  return new Promise(function (resolve, reject) {
-    resolve(79);
-    reject('City not found');
+// promise challenge
+
+function addPromise (a, b) {
+  return new Promise (function (resolve, reject) {
+    if (typeof a === 'number' && typeof b === 'number') {
+    resolve(a + b);
+    } else {
+    reject("I can't add these together!");
+    }
   });
 }
 
-getTempPromise('Philadelphia').then(function (temp) {
-  console.log('promise success', temp);
+addPromise(5, 4).then(function (sum) {
+  console.log('success', sum);
 }, function (err) {
-  console.log('promise error', err);
+  console.log('error', err);
+})
+
+addPromise(3, 'cat').then(function (sum) {
+  console.log('success', sum);
+}, function (err) {
+  console.log('error', err);
 })
