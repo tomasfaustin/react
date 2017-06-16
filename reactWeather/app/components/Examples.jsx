ec0a019135e48511ff17e0a8bfa7584bbaec29fd
Thomas Huisking <tomasfaustin@gmail.com>
2017-06-16 16:58:17 -0700

    Add grid system and style examples component

--- reactWeather/app/components/Examples.jsx
+++ reactWeather/app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
       <div>
-        <h3>Examples</h3>
-        <p>Welcome to the examples page</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few example locations that you can screw around with:</p>
+        <ol>
+          <li>
+          <Link to="/?location=Los Angeles">Los Angeles, California, USA</Link>
+          </li>
+          <li>
+            <Link to="/?location=Montreal">Montreal, Quebec, Canada</Link>
+          </li>
+        </ol>
       </div>
     )
 }
