8c208d408edc644830610c27da659c37dcb6c579
Thomas Huisking <tomasfaustin@gmail.com>
2017-06-17 11:09:48 -0700

    Create new css file and add minimal styling to components

--- reactWeather/app/components/About.jsx
+++ reactWeather/app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>Welcome to the About page. This app was built with the assistance of the incredibly talented <a href="http://www.mead.io/">Andrew Mead</a> and his Udemy React course, which seems to be unavailable at the moment.</p>
       <p>
         Here are some of the tools I used:
