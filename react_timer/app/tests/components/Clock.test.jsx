02f987446d8e77bd3a2b1c4f9fde192042efd781
Thomas Huisking <tomasfaustin@gmail.com>
2017-06-21 13:40:54 -0700

    Add styling to clock component

--- react_timer/app/tests/components/Clock.test.jsx
+++ react_timer/app/tests/components/Clock.test.jsx
@@ -1,7 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var expect = require('expect');
-var $ = require('JQuery');
+var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
 var Clock = require('Clock');
@@ -9,6 +9,16 @@ var Clock = require('Clock');
 describe('Clock', () => {
   it('should exist', () => {
     expect(Clock).toExist();
+  });
+
+  describe('render', () => {
+    it('should render clock to output', () => {
+      var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
+      var $el = $(ReactDOM.findDOMNode(clock));
+      var actualText = $el.find('.clock-text').text();
+
+      expect(actualText).toBe('01:02');
+    });
   });
 
   describe('formatSeconds', () => {
