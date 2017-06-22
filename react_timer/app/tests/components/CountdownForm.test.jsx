808386dcf35b4a51547b6bfe27ccf68102de1e2f
Thomas Huisking <tomasfaustin@gmail.com>
2017-06-22 15:53:00 -0700

    Write test that uses spy

--- react_timer/app/tests/components/CountdownForm.test.jsx
+++ react_timer/app/tests/components/CountdownForm.test.jsx
@@ -10,4 +10,15 @@ describe('CountdownForm', () => {
   it('should exist', () => {
     expect(CountdownForm).toExist();
   });
+
+  it('should call onSetCountdown if valid seconds entered', () => {
+    var spy = expect.createSpy();
+    var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(countdownForm));
+
+    countdownForm.refs.seconds.value = '109';
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toHaveBeenCalledWith(109);
+  });
 });
