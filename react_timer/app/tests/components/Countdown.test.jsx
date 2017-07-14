1ce51c770b0b06e34d77201c95564bd7f27b304a
Thomas Huisking <tomasfaustin@gmail.com>
2017-07-13 18:03:11 -0700

    Write test to ensure reset count on stopped

--- react_timer/app/tests/components/Countdown.test.jsx
+++ react_timer/app/tests/components/Countdown.test.jsx
@@ -45,6 +45,18 @@ describe('Countdown', () => {
          expect(countdown.state.countdownStatus).toBe('paused');
          done();
        }, 1001);
+      });
+
+       it('should reset count on stopped', (done) => {
+         var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+         countdown.handleSetCountdown(3);
+         countdown.handleStatusChange('stopped');
+
+         setTimeout(() => {
+           expect(countdown.state.count).toBe(0);
+           expect(countdown.state.countdownStatus).toBe('stopped');
+           done();
+         }, 1001);
      });
   });
 });
