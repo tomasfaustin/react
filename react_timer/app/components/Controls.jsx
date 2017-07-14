b72873ed50ce98ab596e6ccac3c79c155812050a
Thomas Huisking <tomasfaustin@gmail.com>
2017-07-13 19:01:38 -0700

    Finish building timer logic

--- react_timer/app/components/Controls.jsx
+++ react_timer/app/components/Controls.jsx
@@ -10,13 +10,13 @@ var Controls = React.createClass({
       this.props.onStatusChange(newStatus);
     }
   },
-  
+
   render: function () {
     var {countdownStatus} = this.props;
     var renderStartStopButton = () => {
       if (countdownStatus === 'started') {
         return <button className="button secondary" onClick={this.onStatusChange('paused')}>Pause</button>
-      } else if (countdownStatus === 'paused') {
+      } else {
         return <button className="button primary" onClick={this.onStatusChange('started')}>Start</button>
       }
     };
